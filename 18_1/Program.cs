8a8c4ae350e17206bd51a1c760d6af253beb9951
Marina Savina <fighter_61@bk.ru>
2022-11-23 18:57:26 +0300

    Вывод наименьшего и наибольшего числа

---
+++ 18_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+int Number1 = new int();
+Console.Write("Введите первое число: ");
+Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = new int();
+Console.Write("Введите второе число: ");
+Number2 = Convert.ToInt32(Console.ReadLine());
+if(Number1 < Number2)
+{
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(Number1);   
+    Console.Write("Большее число: ");
+    Console.WriteLine(Number2);
+}
+else
+{
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(Number2);   
+    Console.Write("Большее число: ");
+    Console.WriteLine(Number1);
+}
