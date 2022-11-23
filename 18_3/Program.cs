88d1781362f36f619111eaf1c8317a743b228178
Marina Savina <fighter_61@bk.ru>
2022-11-23 19:24:27 +0300

    Чётность числа

---
+++ 18_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+int Number = new int();
+Console.WriteLine("Является ли число чётным?");
+Console.Write("Введите число: ");
+Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number % 2 == 0);
